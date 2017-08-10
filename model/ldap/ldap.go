c010b3d9435ba3291a28c8665e7cf00d474667f3
Anton Arapov <arapov@gmail.com>
2017-08-10 11:02:21 +0200

    draft implementation of retrieving data from ldap
    OMG! It's not me who wrote it. :)
    
    Signed-off-by: Anton Arapov <arapov@gmail.com>

--- model/ldap/ldap.go
+++ model/ldap/ldap.go
@@ -2,40 +2,116 @@
 package ldap
 
 import (
-	"log"
+	"fmt"
 
 	"gopkg.in/ldap.v2"
 )
 
+/* Assumptions:
+- no person must belong to more than one group
+  - non- UA, TC, PM
+- no person may have more than one role
+*/
+
 type Item struct {
-	dn string
-	cn string
+	mail  string
+	title string
+	cn    string
+	dfg   []string
+	role  []string
 }
+
+type Items map[string]*Item
 
 type Connection interface {
 	Search(*ldap.SearchRequest) (*ldap.SearchResult, error)
 }
 
-func Get(ldapc Connection) ([]Item, error) {
-	var result []Item
+func Get(ldapc Connection) (Items, error) {
+	items := make(Items)
 
+	// TODO: Make it dedup, sane and readable
+
+	// DFGs:
 	searchRequest := ldap.NewSearchRequest(
-		"ou=users,dc=redhat,dc=com", // The base dn to search
-		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
-		"(&(objectClass=organizationalPerson))", // The filter to apply
-		[]string{"dn", "cn"},                    // A list attributes to retrieve
+		"ou=adhoc,ou=managedGroups,dc=redhat,dc=com",
+		ldap.ScopeSingleLevel, ldap.NeverDerefAliases, 0, 0, false,
+		"(&(objectClass=rhatGroup)(cn=rhos-dfg-*))",
+		[]string{"cn", "memberUid"},
+		nil,
+	)
+
+	searchResult, err := ldapc.Search(searchRequest)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, entry := range searchResult.Entries {
+		for _, member := range entry.GetAttributeValues("memberUid") {
+			if _, ok := items[member]; !ok {
+				items[member] = &Item{}
+			}
+			items[member].dfg = append(items[member].dfg, entry.GetAttributeValue("cn"))
+		}
+	}
+
+	// Roles:
+	searchRequest = ldap.NewSearchRequest(
+		"ou=adhoc,ou=managedGroups,dc=redhat,dc=com",
+		ldap.ScopeSingleLevel, ldap.NeverDerefAliases, 0, 0, false,
+		"(&(objectClass=rhatGroup)(|(cn=rhos-ua)(cn=rhos-pm)(cn=rhos-tc)))",
+		[]string{"cn", "memberUid"},
+		nil,
+	)
+
+	searchResult, err = ldapc.Search(searchRequest)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, entry := range searchResult.Entries {
+		for _, member := range entry.GetAttributeValues("memberUid") {
+			if _, ok := items[member]; !ok {
+				items[member] = &Item{}
+			}
+			items[member].role = append(items[member].role, entry.GetAttributeValue("cn"))
+		}
+	}
+
+	// People:
+	filter := "(&(objectClass=rhatPerson)(|"
+	for member := range items {
+		// "(&(objectClass=rhatPerson)(|(uid=user1)(uid=user2)(uid=user3)))"
+		filter = filter + fmt.Sprintf("(uid=%s)", member)
+	}
+	filter = filter + "))"
+
+	searchRequest = ldap.NewSearchRequest(
+		"ou=users,dc=redhat,dc=com",
+		ldap.ScopeSingleLevel, ldap.NeverDerefAliases, 0, 0, false,
+		filter, // The filter to apply
+		[]string{"uid", "mail", "title", "cn"},
 		nil,
 	)
 
 	sr, err := ldapc.Search(searchRequest)
 	if err != nil {
-		// TODO: Must not .Fatal
-		log.Fatal(err)
+		fmt.Println(err)
 	}
 
+	var uid string
 	for _, entry := range sr.Entries {
-		result = append(result, Item{entry.DN, entry.GetAttributeValue("cn")})
+		uid = entry.GetAttributeValue("uid")
+		items[uid].mail = entry.GetAttributeValue("mail")
+		items[uid].title = entry.GetAttributeValue("title")
+		items[uid].cn = entry.GetAttributeValue("cn")
 	}
 
-	return result, err
+	/*
+		for i := range items {
+			fmt.Printf("%s :", i)
+			fmt.Printf("%+v\n", items[i])
+		}
+	*/
+	return items, err
 }
