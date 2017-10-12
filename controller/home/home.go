ed239af4467d25d827cef653c7034a6b928df4fb
Anton Arapov <arapov@gmail.com>
2017-10-12 18:02:51 +0200

    some preps
    Signed-off-by: Anton Arapov <arapov@gmail.com>

--- controller/home/home.go
+++ controller/home/home.go
@@ -2,46 +2,72 @@
 package home
 
 import (
-	"encoding/json"
 	"net/http"
+	"os"
 
 	"github.com/arapov/pile/lib/flight"
-	"github.com/arapov/pile/model/ldap"
+	"github.com/arapov/pile/model/gitwiki"
 
 	"github.com/blue-jay/core/router"
 )
 
+// TODO:
+// - get the git specifics to the /lib
+
+const (
+	directory = "wiki"
+)
+
+var (
+	uri = "/wiki"
+)
+
 // Load the routes.
 func Load() {
-	router.Get("/", Index)
+	router.Get("/", Redirect)
+	router.Get(uri, Index)
+	router.Get(uri+"/create", Clone)
+	router.Post(uri+"/create", Clone)
+}
+
+// exists returns whether the given file or directory exists or not
+func exists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+// Clone - clones git repo, where the wiki pages are stored
+func Clone(w http.ResponseWriter, r *http.Request) {
+	c := flight.Context(w, r)
+
+	v := c.View.New("home/create")
+	v.Render(w, r)
 }
 
 // Index displays the home page.
 func Index(w http.ResponseWriter, r *http.Request) {
 	c := flight.Context(w, r)
 
-	people, dfgs, err := ldap.GetAll(c.LDAP)
-	if err != nil {
-		c.FlashError(err)
-		return
+	if !exists(directory) {
+		c.Redirect(uri + "/create")
 	}
 
-	jdfgs, err := json.Marshal(dfgs)
-	if err != nil {
-		c.FlashError(err)
-		jdfgs = []byte(`{}`)
-	}
-	jpeople, err := json.Marshal(people)
-	if err != nil {
-		c.FlashError(err)
-		jpeople = []byte(`{}`)
-	}
+	output := gitwiki.GetPage()
 
 	v := c.View.New("home/index")
-	if c.Sess.Values["id"] != nil {
-		v.Vars["first_name"] = c.Sess.Values["first_name"]
-	}
-	v.Vars["dfgs"] = jdfgs
-	v.Vars["people"] = jpeople
+	v.Vars["data"] = output
 	v.Render(w, r)
+}
+
+// Redirect - redirects to uri, we don't have root
+func Redirect(w http.ResponseWriter, r *http.Request) {
+	c := flight.Context(w, r)
+
+	c.Redirect(uri)
 }
---
+++ model/gitwiki/gitwiki.go
@@ -0,0 +1,12 @@
+// Package gitwiki - tbd
+package gitwiki
+
+import (
+	"fmt"
+)
+
+func GetPage() string {
+	fmt.Println("Hello")
+
+	return "Hello"
+}
