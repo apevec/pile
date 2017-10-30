3ff4a0a874ec7281ee7920ac088c86705e576459
Anton Arapov <arapov@gmail.com>
2017-10-30 13:31:16 +0100

    remove unused controllers
    Signed-off-by: Anton Arapov <arapov@gmail.com>

--- controller/controller.go
+++ controller/controller.go
@@ -3,13 +3,9 @@ package controller
 
 import (
 	"github.com/arapov/pile/controller/about"
-	"github.com/arapov/pile/controller/debug"
 	"github.com/arapov/pile/controller/head"
 	"github.com/arapov/pile/controller/home"
 	"github.com/arapov/pile/controller/ldapxrest"
-	"github.com/arapov/pile/controller/login"
-	"github.com/arapov/pile/controller/notepad"
-	"github.com/arapov/pile/controller/register"
 	"github.com/arapov/pile/controller/roster"
 	"github.com/arapov/pile/controller/static"
 	"github.com/arapov/pile/controller/status"
@@ -18,13 +14,9 @@ import (
 // LoadRoutes loads the routes for each of the controllers.
 func LoadRoutes() {
 	about.Load()
-	debug.Load()
-	register.Load()
-	login.Load()
 	home.Load()
 	static.Load()
 	status.Load()
-	notepad.Load()
 	roster.Load()
 	head.Load()
 	ldapxrest.Load()
